004f0335f7afba5ba09e0bbd90f1d1d2b4bc228e
msohail22 <mohd.sohail.biz@gmail.com>
2026-02-28 12:04:09 +0530

    initial structure shell architecture with tooling

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc, char *argv[]) {
-  string s;
-  getline(cin, s);
-  cout << "The value taken as an input using getline() is: " << s << "\n";
-  return 0;
-}
